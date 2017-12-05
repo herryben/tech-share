c2611335b451601d74da71ab39929f7559653b23
%E9%83%9D%E7%91%9E <haorui@xiaomi.com>
2017-12-04 20:20:01 -0800

    update slice file

--- slice.py
+++ slice.py
@@ -7,4 +7,16 @@ print slice_list
 
 print len(slice_list)
 
-print slice_list[0], slice_list[-1], slice_list[0: 2:], type(slice_list[0: 2])
+print slice_list[0], slice_list[-1], slice_list[0: 2:], type(slice_list[0: 2]), slice_list[::-1]
+
+class Myseq:
+    my_list = [ i for i in range(5) ]
+
+    def __getitem__(self, index):
+        return self.my_list[index]
+
+    def __len__(self):
+        return len(self.my_list)
+
+my_list = Myseq()
+print my_list[0: 3], len(my_list) 
